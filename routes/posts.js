b2c4b762031eff13d57b22841b5ee18ee5950a78
Rush Tehrani <rush.tehrani@gmail.com>
2016-08-15 14:17:21 -0700

    populate user for comments on comment save

--- routes/posts.js
+++ routes/posts.js
@@ -54,7 +54,18 @@ router.delete('/:id', function(req, res) {
 router.post('/:postId/comments', function(req, res) {
   function onFind(err, post) {
     function onCommentAdded(post) {
-      res.json(post);
+      console.log('=> 1');
+      Post.populate(post, [
+        { path: 'comments.user', select: 'username' }
+      ])
+      .then(function(post) {
+        console.log('=> 2');
+        res.json(post);
+      })
+      .catch(function(err) {
+        console.log('=> 3');
+        res.status(422).json(err);
+      });
     }
 
     function onError(err) {
